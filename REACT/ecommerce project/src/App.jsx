3e139fb8bdf73615a251a3bbef3727079fa96eae
Adam-Driscoll123 <adamdriscoll05@hotmail.com>
2026-06-25 00:23:25 +0100

    Generate HTML using React

--- REACT/ecommerce project/src/App.jsx
+++ REACT/ecommerce project/src/App.jsx
@@ -1,7 +1,10 @@
 import './index.css'
 import { Routes, Route } from 'react-router';
-import { HomePage } from './pages/HomePage'
-import { CheckoutPage } from './pages/CheckoutPage'
+import { HomePage } from './pages/home/HomePage.jsx'
+import { CheckoutPage } from './pages/checkout/CheckoutPage.jsx'
+import { OrdersPage } from './pages/orders/OrdersPage.jsx'
+import { TrackingPage } from './pages/tracking/TrackingPage.jsx'
+import { PageNotFound } from './pages/PageNotFound.jsx'
 
 
 function App() {
@@ -10,6 +13,9 @@ function App() {
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
     
   )
---
+++ REACT/ecommerce project/src/pages/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import { Header } from '../components/Header.jsx'
+
+export function PageNotFound() {
+  return (
+    <>
+      <Header />
+
+      <div className="home-page">
+        404 Not Found
+      </div>
+    </>
+  );
+}
