211146814c6f7f32f02e92a85b3a86c65e225f3f
Adam-Driscoll123 <adamdriscoll05@hotmail.com>
2026-06-25 20:50:42 +0100

    Generate HTML for delivery options

--- REACT/ecommerce project/src/App.jsx
+++ REACT/ecommerce project/src/App.jsx
@@ -5,14 +5,25 @@ import { CheckoutPage } from './pages/checkout/CheckoutPage.jsx'
 import { OrdersPage } from './pages/orders/OrdersPage.jsx'
 import { TrackingPage } from './pages/tracking/TrackingPage.jsx'
 import { PageNotFound } from './pages/PageNotFound.jsx'
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 
 function App() {
+  const [ cart, setCart ] = useState([])
+
+  useEffect(() => {
+    axios.get('api/cart-items?expand=product')
+      .then((response) => {
+        setCart(response.data)
+      })
+  }, [])
+  
 
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="checkout" element={<CheckoutPage />} />
+      <Route index element={<HomePage cart={cart}/>} />
+      <Route path="checkout" element={<CheckoutPage cart={cart}/>} />
       <Route path="orders" element={<OrdersPage />} />
       <Route path="tracking" element={<TrackingPage />} />
       <Route path="*" element={<PageNotFound />} />
--- REACT/ecommerce project/src/pages/home/HomePage.jsx
+++ REACT/ecommerce project/src/pages/home/HomePage.jsx
@@ -2,23 +2,27 @@ import './HomePage.css'
 import { useEffect, useState } from 'react';
 import { Header } from '../../components/Header.jsx'
 import { Link } from 'react-router';
+import { formatMoney } from '../../utils/money.js';
 import axios from 'axios'
 
-export function HomePage () {
+export function HomePage ({ cart }) {
   const [products, setProducts] = useState([]);
+  
   useEffect(() => {
-    axios.get('http://localhost:3000/api/products')
+    axios.get('api/products')
       .then((response) => {
         setProducts(response.data);
       })
   }, []);
+
+  
   
   return (
     <>
       <title>Ecommerce Project</title>
       <link rel="icon" type="image/svg+xml" href="/public/home-favicon.png" />
 
-      <Header />
+      <Header cart={cart}/>
       
 
       <div className="home-page">
@@ -44,7 +48,7 @@ export function HomePage () {
                 </div>
 
                 <div className="product-price">
-                  ${(product.priceCents / 100).toFixed(2)}
+                  {formatMoney(product.priceCents)}
                 </div>
 
                 <div className="product-quantity-container">
